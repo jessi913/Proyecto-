c6ba1432c4f5197be1dc1668cb823aa0546930cc
Ailin <Ailin@DESKTOP-KMVSAAP>
2024-04-04 23:24:31 -0600

    Hice un comentario

---
+++ Models/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto_.Models
+{
+    public class Cliente
+    {
+        //Comentario
+    }
+}
