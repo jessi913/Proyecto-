024d6607ef6e9e199024bead448453bb332013c0
Jessica Landaverde <119987685+jessi913@users.noreply.github.com>
2024-04-05 23:36:29 -0600

    migraciones

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Proyecto_.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Proyecto_.Database.proyecto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Proyecto_.Database.proyecto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
